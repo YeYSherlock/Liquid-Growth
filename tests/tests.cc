bf574b6f7d1bb5035232a301680c0c24e8c7a3f7
yanzhen4 <yanzhen4@illinois.edu>
2021-12-05 22:55:13 -0600

    finish test case

--- tests/tests.cc
+++ tests/tests.cc
@@ -65,6 +65,80 @@ TEST_CASE("Add shiny") {
     }
 }
 
-TEST_CASE("SHINY") {
+TEST_CASE("Check Vase's vector in collection"){
+    Collection collection;
+    collection.AddAtDate(9,20,2002,1);
+    collection.AddAtDate(9,21,2002,1);
+    collection.AddAtDate(12,5,2021,0);
+    collection.AddAtDate(12,6,2021,0);
 
+    SECTION("Current Vase"){
+        bool check = false;
+        Vase* vase = collection.GetVase();
+        if(vase->GetVase() == vase->Getstamnos()){
+           if(vase->GetDate() == 6 && vase->GetMonth() == 12 && vase->GetYear() == 2021){
+               check = true;
+           } 
+        }
+        REQUIRE(check == true);
+    }
+
+    SECTION("Previous Vase"){
+        bool check = false;
+        collection.Prev();
+        collection.Prev();
+         Vase* vase = collection.GetVase();
+        if(vase->GetVase() == vase->Getkalathos()){
+           if(vase->GetDate() == 21 && vase->GetMonth() == 9 && vase->GetYear() == 2002){
+               check = true;
+           } 
+        }
+        REQUIRE(check == true);
+    }
+
+    SECTION("Next Vase"){
+        bool check = false;
+        collection.Prev();
+        collection.Prev();
+        collection.Next();
+        Vase* vase = collection.GetVase();
+        if(vase->GetVase() == vase->Getstamnos()){
+           if(vase->GetDate() == 5 && vase->GetMonth() == 12 && vase->GetYear() == 2021){
+               check = true;
+           } 
+        }
+        REQUIRE(check == true);
+    }
+}
+
+TEST_CASE("Delete Shiny and undo"){
+    SECTION("Vase Delete Shiny"){
+        Vase* vase = new Vase(0, 12, 25, 2002, "Monday", "Happy", "Rain");
+        vase->addShiny(25,"$","MP");
+        vase->addShiny(20,"!","TRA");
+        vase->DeleteLastShiny();
+        REQUIRE(vase->GetShinyvector().size() == 1);
+    }
+
+    SECTION("Collection undo"){
+        Collection collection;
+        collection.AddAtDate(9,20,2002,1);
+        collection.Add(25,"$","MP");
+        collection.Undo();
+        REQUIRE(collection.GetVase()->GetShinyvector().size() == 0);
+    }
+}
+
+TEST_CASE("Save and Load"){
+    bool check = false;
+    Vase* original_vase = new Vase(0, 12, 25, 2002, "Monday", "Happy", "Rain");
+    original_vase->SaveFile();
+    Vase* loaded_vase = new Vase(1, 1, 1,2002, "Sunday", "Sad", "Sunny");
+    loaded_vase->LoadFile("./storage/12_25_2002");
+    if(loaded_vase->GetVase() == loaded_vase->Getstamnos()){
+        if(loaded_vase->GetDate() == 25 && loaded_vase->GetMonth() == 12 && loaded_vase->GetYear() == 2002){
+            check = true;
+        } 
+    }
+    REQUIRE(check == true);
 }
